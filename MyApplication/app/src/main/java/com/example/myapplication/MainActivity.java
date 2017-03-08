6d989d71dc1a117b2b8d3545f6887555417c2242
gilax <gilad.wolf@mail.huji.ac.il>
2017-03-08 18:53:55 +0200

    Merge branch 'master' of https://github.com/gilax/PostPC add font changer

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,7 +20,11 @@ public class MainActivity extends AppCompatActivity {
         Button button = (Button) findViewById(R.id.button);
         final EditText editText = (EditText) findViewById(R.id.message_edit_text);
         final TextView textView = (TextView) findViewById(R.id.textView);
+        final Switch options = (Switch) findViewById(R.id.options);
+        final RadioGroup fonts = (RadioGroup) findViewById(R.id.chooser);
+
         textView.append("Your last messages are:\n");
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,17 +33,36 @@ public class MainActivity extends AppCompatActivity {
                 textView.append(message + "\n");
             }
         });
-        final Switch visibility = (Switch) findViewById(R.id.colorText);
-        visibility.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+        options.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 Toast.makeText(getApplicationContext(),
-                        "You turn the last messages to " + (isChecked? "visible":"invisible"),
+                        "You turn the options to " + (isChecked? "visible":"invisible"),
                         Toast.LENGTH_SHORT).show();
                 if (isChecked) {
-                    textView.setVisibility(View.VISIBLE);
-                } else {
                     textView.setVisibility(View.INVISIBLE);
+                } else {
+                    textView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        fonts.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (group.getCheckedRadioButtonId()) {
+                    case R.id.bigFont:
+                        textView.setTextSize(R.dimen.BigTextSize);
+                        break;
+                    case R.id.mediumFont:
+                        textView.setTextSize(R.dimen.MediumTextSize);
+                        break;
+                    case R.id.smallFont:
+                        textView.setTextSize(R.dimen.SmallTextSize);
+                        break;
+                    default:
+                        break;
                 }
             }
         });
